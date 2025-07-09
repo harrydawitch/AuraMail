80d5ca62a9808f0fbc74d29035fc1fba06c44a76
HarryDaWitch <137988388+harrydawitch@users.noreply.github.com>
2025-07-09 13:59:54 +0700

    Delete tools.py

--- tools.py
+++
@@ -1,7 +0,0 @@
-from langchain_core.tools import tool
-from langchain_google_community.gmail.send_message import GmailSendMessage
-from langchain_google_community.gmail import GmailToolKit
-
-@tool("send_gmail")
-def send_email():
-    pass
